b1f79f3ea04fa28b34ec69718717c3d8d0682bee
Mogget <karol.skoruch@gmail.com>
2012-03-05 23:01:30 +0100

    change

--- config.py
+++ config.py
@@ -19,21 +19,29 @@ class Config(object):
         with open(self.configName, "wb") as fileConfig:
             # self.serialDict = Config.serial.getSettingsDict()
             cPickle.dump(self.serialDict, fileConfig)
+            try:
+                Config.serial.port = self.serialDict['port']
+            except KeyError:
+                pass
             Config.serial.applySettingsDict(self.serialDict)
 
     def load(self):
         with open(self.configName, "rb") as fileConfig:
             self.serialDict = cPickle.load(fileConfig)
+            try:
+                Config.serial.port = self.serialDict['port']
+            except KeyError:
+                pass
             Config.serial.applySettingsDict(self.serialDict)
 
 if __name__ == "__main__":
     configTest = Config()
-    configTest.load()
-    print configTest.serial
-    print configTest.serialDict
+    # configTest.load()
+    # print configTest.serial
+    # print configTest.serialDict
     # configTest.save()
     # configTest.load()
-    # print configTest.serialDict
+    print configTest.serialDict
     # configTest.serial.baudrate = 19200
     # configTest.save()
     # del configTest
